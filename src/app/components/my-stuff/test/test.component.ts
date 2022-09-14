09f3cbd5b1b49549d772299fb232d940b70ef6c3
Simon Schwegler <simon.schwegler@basenet.ch>
2022-09-14 15:56:54 +0200

    Cleanup

--- src/app/components/my-stuff/test/test.component.ts
+++ src/app/components/my-stuff/test/test.component.ts
@@ -35,49 +35,15 @@ export class TestComponent implements OnInit {
         LastName: 'User',
       }
     ).subscribe(() => console.log('done'));
-
-    // let person = await firstValueFrom(serviceWithParser
-    //   .create({
-    //     Emails: ['some@email.com'],
-    //     UserName: 'someuser',
-    //     Gender: PersonGender.Male,
-    //     FirstName: 'Some',
-    //     LastName: 'User',
-    //   })
-    //   .pipe(
-    //     map(({ entity, annots }) => {
-    //       etag = annots.etag;
-    //       return entity;
-    //     })
-    //   ));
-    // console.log(person, etag);
-
-
-    // this.peopleService.fetchAll().subscribe(console.log);
-   //  this.person.fetchAll().subscribe(console.log);
   }
 
   modifyPerson()
   {
-    /*
-     .modify('someuser', { LastName: 'LastName' }, { etag })
-        .pipe(
-          map(({ entity, annots }) => {
-            etag = annots.etag;
-            return entity;
-          })
-        ));
-    */
-
-    // this.peopleService.modify('someuser', { LastName: 'LastName Updated'}).subscribe(() => console.log('done'));
-
-
-        this.peopleService.modify('someuser', { LastName: 'LastName' })
-        .pipe(
-          map(({ entity, annots }) => {
-            return entity;
-          }))
-        .subscribe(() => console.log('done'));
+    this.peopleService.modify('someuser', { LastName: 'LastName' })
+    .pipe(
+      map(({ entity, annots }) => {
+        return entity;
+      }))
+    .subscribe(() => console.log('done'));
   }
-
 }
