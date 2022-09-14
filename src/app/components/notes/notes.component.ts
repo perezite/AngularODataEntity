49acf82d23bd52bdd350ab917079dec29102c140
Simon Schwegler <simon.schwegler@basenet.ch>
2022-09-14 18:52:34 +0200

    Add notes patch

--- src/app/components/notes/notes.component.ts
+++ src/app/components/notes/notes.component.ts
@@ -1,5 +1,6 @@
-import { NotesService } from './../../notes/Default/notes.service';
 import { Component, OnInit } from '@angular/core';
+import { NotesService } from './../../notes/Default/notes.service';
+import { Note } from 'src/app/notes/ODataTutorial/Entities/note.entity';
 
 @Component({
   selector: 'app-notes',
@@ -7,6 +8,8 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./notes.component.css']
 })
 export class NotesComponent implements OnInit {
+  notes: Note[] = [];
+
   constructor(private notesService: NotesService) { }
 
   ngOnInit(): void {
@@ -15,6 +18,25 @@ export class NotesComponent implements OnInit {
 
   fetchNotes() {
     console.clear();
-    this.notesService.entities().fetchAll().subscribe(x => console.log(x));
+    this.notesService.entities().fetchAll().subscribe(notes => {
+      this.notes = notes;
+      console.log(notes);
+    });
   }
+
+  updateNote(id: string) {
+    let message = this.randomString(20);
+    this.notesService.modify(id, { MessageNote: message })
+      .subscribe(_ => console.log('Update done!'));
+  }
+
+  randomString(length: number) {
+    let randomChars = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
+    let result = '';
+    for ( var i = 0; i < length; i++ ) {
+        result += randomChars.charAt(Math.floor(Math.random() * randomChars.length));
+    }
+
+    return result;
+}
 }
